14add1f609ba97dca2aca922e344e1c2d51eeb0a
jmesyan <jieqiang1017@qq.com>
2017-03-22 02:25:40 +0800

    标准模式-golang

--- Filter/Filter.go
+++ Filter/Filter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"container/list"
 	"strings"
+	"fmt"
 )
 
 type Person struct {
@@ -27,16 +28,15 @@ type Criteria interface {
 	meetCriteria(persons list.List) list.List
 }
 
-
 type CriteriaMale struct {
 
 }
 
 func (c *CriteriaMale)meetCriteria(persons list.List) list.List {
-	malePersons := list.New()
+	var malePersons list.List
 	for p := persons.Front(); p != nil; p = p.Next() {
-		person := p.Value()
-		gender := strings.ToLower(person.(Person).getGender())
+		person := p.Value.(*Person)
+		gender := strings.ToLower(person.getGender())
 		if (gender == "male") {
 			malePersons.PushBack(person)
 		}
@@ -49,10 +49,10 @@ type CriteriaFemale struct {
 }
 
 func (c *CriteriaFemale)meetCriteria(persons list.List) list.List {
-	femalePersons := list.New()
+	var femalePersons list.List
 	for p := persons.Front(); p != nil; p = p.Next() {
-		person := p.Value()
-		gender := strings.ToLower(person.(Person).getGender())
+		person := p.Value.(*Person)
+		gender := strings.ToLower(person.getGender())
 		if (gender == "female") {
 			femalePersons.PushBack(person)
 		}
@@ -65,10 +65,10 @@ type CriteriaSingle struct {
 }
 
 func (c *CriteriaSingle)meetCriteria(persons list.List) list.List {
-	singlePersons := list.New()
+	var singlePersons list.List
 	for p := persons.Front(); p != nil; p = p.Next() {
-		person := p.Value()
-		maritalStatus := strings.ToLower(person.(Person).getMaritalStatus())
+		person := p.Value.(*Person)
+		maritalStatus := strings.ToLower(person.getMaritalStatus())
 		if (maritalStatus == "single") {
 			singlePersons.PushBack(person)
 		}
@@ -77,28 +77,77 @@ func (c *CriteriaSingle)meetCriteria(persons list.List) list.List {
 }
 
 type AndCriteria struct {
-	criteria Criteria
+	criteria      Criteria
 	otherCriteria Criteria
 }
 
-func(a *AndCriteria)meetCriteria(persons list.List) list.List{
-	firstCriteriaPersons:=a.criteria.meetCriteria(persons)
+func (a *AndCriteria)meetCriteria(persons list.List) list.List {
+	firstCriteriaPersons := a.criteria.meetCriteria(persons)
 	return a.otherCriteria.meetCriteria(firstCriteriaPersons)
 }
 
 type OrCriteria struct {
-	criteria Criteria
+	criteria      Criteria
 	otherCriteria Criteria
 }
 
-func(o *OrCriteria)meetCriteria(persons list.List) list.List{
-	firstCriteriaPersons:=o.criteria.meetCriteria(persons)
-	otherCriteriaPersons:=o.otherCriteria.meetCriteria(persons)
-	for p:=otherCriteriaPersons.Front();p!=nil;p=p.Next(){
-		person:=p.Value()
-
+func (o *OrCriteria)meetCriteria(persons list.List) list.List {
+	firstCriteriaPersons := o.criteria.meetCriteria(persons)
+	otherCriteriaPersons := o.otherCriteria.meetCriteria(persons)
+	for p := otherCriteriaPersons.Front(); p != nil; p = p.Next() {
+		person := p.Value.(*Person)
+		contains, _ := Contains(firstCriteriaPersons, person)
+		if (!contains) {
+			firstCriteriaPersons.PushBack(person)
+		}
 
 	}
+	return firstCriteriaPersons
+}
+
+func Contains(l list.List, value *Person) (bool, *list.Element) {
+	for e := l.Front(); e != nil; e = e.Next() {
+		if (e.Value == value) {
+			return true, e
+		}
+	}
+	return false, nil
+}
+
+func printPersons(p list.List) {
+	for e := p.Front(); e != nil; e = e.Next() {
+		person := e.Value.(*Person)
+		fmt.Println("Person : [ Name : ", person.getName(), ", Gender : ", person.getGender(), ", Marital Status : ", person.getMaritalStatus(), " ]")
+	}
+}
+
+func main() {
+	//demo
+	var persons list.List;
+	persons.PushBack(&Person{"Robert", "Male", "Single"})
+	persons.PushBack(&Person{"John", "Male", "Married"})
+	persons.PushBack(&Person{"Laura", "Female", "Married"})
+	persons.PushBack(&Person{"Diana", "Female", "Single"})
+	persons.PushBack(&Person{"Mike", "Male", "Single"})
+	persons.PushBack(&Person{"Bobby", "Male", "Single"})
+
+	male := new(CriteriaMale)
+	fmt.Println("Males: ")
+	printPersons(male.meetCriteria(persons))
+
+	female := new(CriteriaFemale)
+	fmt.Println("Females: ")
+	printPersons(female.meetCriteria(persons))
+
+	single := new(CriteriaSingle)
+
+	singleMale := &AndCriteria{single, male}
+	fmt.Println("singleMales: ")
+	printPersons(singleMale.meetCriteria(persons))
+
+	singleOrFemale := &OrCriteria{single, female}
+	fmt.Println("single or females: ")
+	printPersons(singleOrFemale.meetCriteria(persons))
 }
 
 
